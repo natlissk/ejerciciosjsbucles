d82b04b3baa3dc0ff658c04b0467341126f24d9d
LAPT -CSH-natlissk <monterrozanatalia3@gmail.com>
2025-03-18 19:01:40 -0600

    bucles

---
+++ Bucles for/main.js
@@ -0,0 +1,12 @@
+//1.1: Imprime los números del 1 al 10 usando for.
+for i in Range(1,11)
+print i
+
+//1.2: Imprime los números pares entre 0 y 20 usando for y una condición if.
+for i in range (0,20)
+for i % 2 == 0:
+print (i)
+
+//1.3 (Desafío): Crea un triángulo con asteriscos usando bucles anidados:
+for i in range (1,6)
+print ('*'*i)
